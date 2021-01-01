4e60e2397f8b436e3056ea022c9edfa05f30486a
Tamar <mischegoss@gmail.com>
2021-01-01 11:16:19 -0500

    Add fade effect

--- assets/scripts.js
+++ assets/scripts.js
@@ -33,7 +33,20 @@ async function fetchCount() {
         document.getElementById("image-text").textContent=total;
 
         function hideOverlay() {
-       document.getElementById("overlay").style.display = "none";
+       var fadeTarget = document.getElementById("overlay");
+       var fadeEffect = setInterval(function () {
+           if (!fadeTarget.style.opacity) {
+               fadeTarget.style.opacity = 1;
+           }
+           if (fadeTarget.style.opacity > 0) {
+               fadeTarget.style.opacity -= 0.1;
+           } else {
+               clearInterval(fadeEffect);
+           }
+       }, 200);
+
+
+
         }
 
         setTimeout(hideOverlay, 4500)
